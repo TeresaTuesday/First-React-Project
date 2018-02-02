fdcc7180c2f65d097fc889989f96bbfbe4ee3aba
Teresa Tuesday <teresatuesday@TERESAs-MacBook-Air.local>
2018-02-02 14:55:14 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>My name is Teresa, but you can call me T-Dawg.</h1>
+          <ul>
+            <li>I like to eat potatoes</li>
+            <li>In the past I was a pizza and chinese food delivery driver</li>
+            <li>I am a lady and a scholar</li>
+          </ul>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
